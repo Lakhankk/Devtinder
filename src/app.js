9a2bf446244cb30d03a7a1ff50de856f3efe4642
lakhankk <lakhankharade04@gmail.com>
2025-02-23 20:32:51 +0530

    created a express server

--- src/app.js
+++ src/app.js
@@ -2,16 +2,20 @@ const express = require(`express`);
 
 const app = express();
 
-app.use("/home", (req,res) => {
-    res.send("hello tooo");
+app.use("/hello/2", (req,res) => {
+    res.send("abarakadabra")
 });
 
-app.use("/home2", (req,res)=> {
-    res.send( "hello threee");
+app.use("/hello", (req,res) => {
+    res.send("hello hello hello");
 });
 
-app.use((req,res) => {
-    res.send("hello");
+app.use("/test", (req,res)=> {
+    res.send( "hello ");
+});
+
+app.use("/", (req,res) => {
+    res.send("Namaste Lakhan");
 });
 
 app.listen(7777,() =>{
