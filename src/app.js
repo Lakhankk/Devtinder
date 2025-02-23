3988e0c82044c6f6a2c09a6da2139ca3663607a4
lakhankk <lakhankharade04@gmail.com>
2025-02-23 23:05:24 +0530

    playingbwith routes

--- src/app.js
+++ src/app.js
@@ -2,28 +2,9 @@ const express = require(`express`);
 
 const app = express();
 
-app.use("/user", (req,res) => {
-    res.send("hahahhahahahaha")
-});
-
-app.get("/user", (req,res) => {
-    res.send({firstName : "Lakhan", lastName : "Kharade"})
-});
-
-app.post("/user", (req,res) => {
-    res.send("data upload successfully")
-});
-
-app.patch("/user", (req,res) => {
-    res.send("user name updated")
-});
-
-app.put("/user", (req,res) => {
-    res.send("data upadated sucessfully")
-});
-
-app.delete("/user", (req,res) => {
-    res.send("user delete secussfully")
+app.get("/user/:userId/:name/:password", (req,res) => {
+    console.log(req.params)
+    res.send({firstName : "Lakhan", lastName: "Kharade"})
 });
 
 app.listen(7777,() =>{
